623b0c91c2418cf1ed365a600ae793c077264bde
Vladimir Zorin <vladimir@deviant.guru>
2019-05-28 16:23:29 +0300

    Fix selfie.difftime function

--- lua/libs/selfie/selfie.lua
+++ lua/libs/selfie/selfie.lua
@@ -1,15 +1,19 @@
 local selfie = {}
 
 -- Returns the difference between two points
--- in time. Args should be tables as returned by core.now() function.
-selfie.difftime = function(t2,t1,as_string)
+-- in time in seconds (with millisecond presicion). Args should be tables as returned by core.now() function.
+selfie.difftime = function(t2, t1, as_string)
 
-     local seconds = math.abs(t2.sec - t1.sec)
-     local milliseconds = math.floor(math.abs( (t2.usec - t1.usec)/1000 ))
-     if as_string then
-         return seconds.."."..milliseconds
-     end
-     return seconds, milliseconds
+    if t2 and t2.sec and t2.usec and t1 and t1.sec and t1.usec then
+       local seconds = t2.sec - t1.sec + (t2.usec - t1.usec) / 1000000
+       local seconds_string = string.format("%.3f", seconds)
+       if as_string then
+            return seconds_string
+       else
+            return tonumber(seconds_string)
+       end
+    end
+    return nil, "wrong format of arguments"
 
 end
 
